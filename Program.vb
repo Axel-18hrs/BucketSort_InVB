643dbd55ab0f35ef83b99ed0f07165cf0c627b5a
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-12-07 18:22:27 -0600

    .

--- Program.vb
+++ Program.vb
@@ -1,143 +1,132 @@
 Imports System
 Imports System.Runtime.InteropServices
 Imports System.Runtime.InteropServices.JavaScript.JSType
+Imports System.Runtime.Serialization
 
-Module Module1
+Module Program
     Private _rand As New Random()
 
-    Sub Main()
+    Sub Main(args As String())
         Do
             Console.Clear()
-            Console.WriteLine("Seleccione una opción:")
-            Console.WriteLine("1. Utilizar el arreglo directamente (números enteros)")
-            Console.WriteLine("2. Utilizar el método que genera un vector (números enteros)")
-            Console.WriteLine("3. Utilizar el arreglo directamente (números decimales)")
-            Console.WriteLine("4. Utilizar el método que genera un vector (números decimales)")
-            Console.WriteLine("0. Salir")
-
-            Dim opcion As Integer
-            If Not Integer.TryParse(Console.ReadLine(), opcion) Then
-                Console.WriteLine("Entrada no válida. Por favor, ingrese un número válido.")
+            Console.WriteLine("Select an option:")
+            Console.WriteLine("1. Use the array directly (integer numbers)")
+            Console.WriteLine("2. Use the method that generates an array (integer numbers)")
+            Console.WriteLine("3. Use the array directly (decimal numbers)")
+            Console.WriteLine("4. Use the method that generates an array (decimal numbers)")
+            Console.WriteLine("0. Exit")
+            Dim opc As Integer
+            If Not Integer.TryParse(Console.ReadLine(), opc) Then
+                Console.WriteLine("Invalid input. Please enter a valid number.")
                 Console.ReadKey()
                 Continue Do
             End If
 
-            Select Case opcion
+            Select Case opc
                 Case 1
                     Dim array1() As Integer = {4, 2, 3, 5, 5, 7, 1}
-                    Console.WriteLine("Array antes de ordenar:")
-                    PrintArray(array1)
+                    Console.WriteLine("Array before sorting: [" & String.Join(", ", array1) & "]")
+                    Dim startTime As DateTime = DateTime.Now
                     BucketSort_int(array1)
-                    Console.WriteLine(vbCrLf & "Array después de ordenar:")
-                    PrintArray(array1)
-                    Exit Select
-
+                    Console.WriteLine(vbLf & "Array after sorting: [" & String.Join(", ", array1) & "]")
+                    Console.WriteLine("Time: " & (DateTime.Now - startTime).ToString())
                 Case 2
-                    Dim array2() As Integer = GenerarVector()
-                    Console.WriteLine("Array antes de ordenar:")
-                    PrintArray(array2)
+                    Dim array2() As Integer = GenerateIntArray()
+                    Console.WriteLine("Array before sorting: [" & String.Join(", ", array2) & "]")
+                    Dim startTime_ As DateTime = DateTime.Now
                     BucketSort_int(array2)
-                    Console.WriteLine(vbCrLf & "Array después de ordenar:")
-                    PrintArray(array2)
-                    Exit Select
-
+                    Console.WriteLine(vbLf & "Array after sorting: [" & String.Join(", ", array2) & "]")
+                    Console.WriteLine("Time: " & (DateTime.Now - startTime_).ToString())
                 Case 3
                     Dim array3() As Double = {0.42, 0.33, 0.37, 0.57, 0.4}
-                    Console.WriteLine("Array antes de ordenar:")
-                    PrintArray(array3)
+                    Console.WriteLine("Array before sorting: [" & String.Join(", ", array3) & "]")
+                    Dim startTim As DateTime = DateTime.Now
                     BucketSort_Double(array3)
-                    Console.WriteLine(vbCrLf & "Array después de ordenar:")
-                    PrintArray(array3)
-                    Exit Select
-
+                    Console.WriteLine(vbLf & "Array after sorting: [" & String.Join(", ", array3) & "]")
+                    Console.WriteLine("Time: " & (DateTime.Now - startTim).ToString())
                 Case 4
-                    Dim array4() As Double = GenerarVectorDouble()
-                    Console.WriteLine("Array antes de ordenar:")
-                    PrintArray(array4)
+                    Dim array4() As Double = GenerateDoubleArray()
+                    Console.WriteLine("Array before sorting: [" & String.Join(", ", array4) & "]")
+                    Dim s_tartTim As DateTime = DateTime.Now
                     BucketSort_Double(array4)
-                    Console.WriteLine(vbCrLf & "Array después de ordenar:")
-                    PrintArray(array4)
-                    Exit Select
-
+                    Console.WriteLine(vbLf & "Array after sorting: [" & String.Join(", ", array4) & "]")
+                    Console.WriteLine("Time: " & (DateTime.Now - s_tartTim).ToString())
                 Case 0
                     Return
-
                 Case Else
-                    Console.WriteLine("Opción no válida. Por favor, elija una opción del 0 al 4.")
+                    Console.WriteLine("Invalid option. Please choose an option from 0 to 4.")
             End Select
-
             Console.ReadKey()
         Loop While True
     End Sub
 
-    Public Function GenerarVectorDouble(Optional ByVal Minon As Integer = 0, Optional ByVal Lenght As Integer = 10, Optional ByVal values As Integer = 5) As Double()
-        Dim _List As New List(Of Double)()
+    Public Function GenerateDoubleArray(Optional Min As Integer = 0, Optional Length As Integer = 10, Optional Values As Integer = 5) As Double()
+        Dim list As New List(Of Double)()
 
-        For i As Integer = Minon To Lenght - 1
-            If i < values Then
-                Dim NewValor As Double = _rand.NextDouble()
-                _List.Add(NewValor)
-            Else
-                Exit For
-            End If
-        Next
-        Return _List.ToArray()
-    End Function
-
-    Public Function GenerarVector(Optional ByVal Minon As Integer = 0, Optional ByVal Lenght As Integer = 10, Optional ByVal values As Integer = 5) As Integer()
-        Dim _List As New List(Of Integer)()
-
-        For i As Integer = Minon To Lenght - 1
-            If i < values Then
-                Dim NewValor As Integer = _rand.Next(Minon, Lenght + 1)
-                If _List.Contains(NewValor) Then
+        For i As Integer = Min To Length - 1
+            If i < Values Then
+                Dim newValue As Double = _rand.NextDouble()
+                If list.Contains(newValue) Then
                     i -= 1
                     Continue For
                 End If
-                _List.Add(NewValor)
+                list.Add(newValue)
             Else
                 Exit For
             End If
         Next
-        Return _List.ToArray()
+        Return list.ToArray()
     End Function
 
-    Private Sub PrintArray(ByVal doubles As Double())
+    Public Function GenerateIntArray(Optional Min As Integer = 0, Optional Length As Integer = 10, Optional Values As Integer = 5) As Integer()
+        Dim list As New List(Of Integer)()
+
+        For i As Integer = Min To Length - 1
+            If i < Values Then
+                Dim newValue As Integer = _rand.Next(Min, Length + 1)
+                If list.Contains(newValue) Then
+                    i -= 1
+                    Continue For
+                End If
+                list.Add(newValue)
+            Else
+                Exit For
+            End If
+        Next
+        Return list.ToArray()
+    End Function
+
+    Private Sub PrintArray(doubles As Double())
         For Each item As Double In doubles
             Console.Write(item & " ")
         Next
         Console.WriteLine()
     End Sub
 
-    Sub BucketSort_Double(ByVal array As Double())
-        ' Crear buckets vacíos
+    Sub BucketSort_Double(array As Double())
+        ' Create empty buckets
         Dim buckets(array.Length - 1) As List(Of Double)
         For i As Integer = 0 To buckets.Length - 1
             buckets(i) = New List(Of Double)()
         Next
 
-        ' Insertar elementos en sus respectivos buckets
+        ' Insert elements into their respective buckets
         For Each element As Double In array
-            Dim bucketIndex As Integer = CInt(Math.Floor(element * array.Length))
-            ' Asegurarse de que el índice del bucket esté dentro de los límites del array
-            If bucketIndex >= array.Length Then
-                bucketIndex = array.Length - 1
-            End If
-            buckets(bucketIndex).Add(element)
+            buckets(CInt(Fix(element * array.Length))).Add(element)
         Next
 
-        ' Imprimir el estado de los buckets después de la inserción
+        ' Print the state of the buckets after insertion
         PrintBucketState(buckets)
 
-        ' Ordenar los elementos de cada cubo
+        ' Sort the elements in each bucket
         For i As Integer = 0 To array.Length - 1
             buckets(i).Sort()
         Next
 
-        ' Imprimir el estado de los buckets después de la ordenación
+        ' Print the state of the buckets after sorting
         PrintBucketState(buckets)
 
-        ' Obtener los elementos ordenados
+        ' Get the sorted elements
         Dim k As Integer = 0
         For i As Integer = 0 To array.Length - 1
             For Each item As Double In buckets(i)
@@ -147,8 +136,19 @@ Module Module1
         Next
     End Sub
 
+    Sub PrintBucketState(buckets As List(Of Integer)())
+        Console.WriteLine("Current state of buckets:")
+        For i As Integer = 0 To buckets.Length - 1
+            Console.Write($"Bucket {i}: ")
+            For Each item As Integer In buckets(i)
+                Console.Write($"{item} ")
+            Next
+            Console.WriteLine()
+        Next
+        Console.WriteLine()
+    End Sub
 
-    Sub PrintBucketState(ByVal buckets() As List(Of Double))
+    Sub PrintBucketState(buckets As List(Of Double)())
         Console.WriteLine("Current state of buckets:")
         For i As Integer = 0 To buckets.Length - 1
             Console.Write($"Bucket {i}: ")
@@ -160,20 +160,8 @@ Module Module1
         Console.WriteLine()
     End Sub
 
-    Sub PrintBucketState(ByVal buckets() As List(Of Integer))
-        Console.WriteLine("Current state of buckets:")
-        For i As Integer = 0 To buckets.Length - 1
-            Console.Write($"Bucket {i}: ")
-            For Each item As Double In buckets(i)
-                Console.Write($"{item} ")
-            Next
-            Console.WriteLine()
-        Next
-        Console.WriteLine()
-    End Sub
-
-    Public Sub BucketSort_int(ByVal array As Integer())
-        ' Encuentra el valor máximo en el array
+    Public Sub BucketSort_int(array As Integer())
+        ' Find the maximum value in the array
         Dim maxVal As Integer = array(0)
 
         For i As Integer = 1 To array.Length - 1
@@ -181,26 +169,25 @@ Module Module1
                 maxVal = array(i)
             End If
         Next
-
-        ' Crea una lista de buckets vacíos
+        ' Create a list of empty buckets
         Dim buckets(maxVal) As List(Of Integer)
         For i As Integer = 0 To buckets.Length - 1
             buckets(i) = New List(Of Integer)()
         Next
 
-        ' Distribuye los elementos en los buckets
+        ' Distribute elements into the buckets
         For i As Integer = 0 To array.Length - 1
             buckets(array(i)).Add(array(i))
         Next
         PrintBucketState(buckets)
 
-        ' Ordena cada cubo individualmente
+        ' Sort each bucket individually
         For i As Integer = 0 To buckets.Length - 1
             buckets(i).Sort()
         Next
         PrintBucketState(buckets)
 
-        ' Concatena los elementos ordenados de cada cubo
+        ' Concatenate the sorted elements from each bucket
         Dim index As Integer = 0
         For i As Integer = 0 To buckets.Length - 1
             For Each item As Integer In buckets(i)
@@ -210,7 +197,7 @@ Module Module1
         Next
     End Sub
 
-    Sub PrintArray(ByVal arr As Integer())
+    Sub PrintArray(arr As Integer())
         For Each item As Integer In arr
             Console.Write(item & " ")
         Next
